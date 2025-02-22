280c46b4b2a474c3d807fbb551420e11384523ef
Nina Lopez Laudenbach <nina.laudenbach@udc.es>
2025-02-22 16:17:18 +0100

    feature: table creation (users, diary, personal_profile)

---
+++ init.sql
@@ -0,0 +1,24 @@
+CREATE TABLE users (
+    token VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    contraseña VARCHAR(255) NOT NULL,
+    PRIMARY KEY (email)
+);
+
+CREATE TABLE diary (
+    user_email VARCHAR(255) REFERENCES users(email),
+    date DATE NOT NULL,
+    text TEXT NOT NULL,
+    emotion VARCHAR(255) NOT NULL,
+    PRIMARY KEY (user_email, date)
+);
+
+CREATE TABLE personal_profile (
+    user_email VARCHAR(255) REFERENCES users(email),
+    score_neuroticismo DECIMAL NOT NULL,
+    score_extroversion DECIMAL NOT NULL,
+    score_agreeableness DECIMAL NOT NULL,
+    score_conscientiousness DECIMAL NOT NULL,
+    score_openness DECIMAL NOT NULL,
+    PRIMARY KEY (user_email)
+);
